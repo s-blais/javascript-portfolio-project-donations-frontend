5be2ce4ed25f3feb2693196ceda8b0f7e4a11102
s-blais <stephen@s-blais.com>
2020-12-10 10:28:54 -0500

    add createTransaction fetch and handle response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   // fetch and render existing Transactions from db
   fetchTransactions()
-  // set create Transaction form and set listener and handler
+  // set create Transaction form variable and set listener and handler onto it
   const createTransactionForm = document.getElementById('create-transaction-form')
   createTransactionForm.addEventListener('submit', (e) => createTransactionHandler(e))
 })
@@ -46,8 +46,22 @@ function createTransactionHandler(e) {
   const date = document.querySelector('#create-transaction-date').value
   const recipient = document.querySelector('#create-transaction-recipient').value
   const contact = document.querySelector('#create-transaction-contact').value
-  const amount = document.querySelector('#create-transaction-amount').value
-  const fund = document.querySelector('#create-transaction-fund').value
+  const amount = parseInt(document.querySelector('#create-transaction-amount').value)
+  const fund_id = parseInt(document.querySelector('#create-transaction-fund').value)
   const notes = document.querySelector('#create-transaction-notes').value
-  debugger
+  //debugger
+  createTransactionFetch(date, recipient, contact, amount, fund_id, notes)
+}
+
+function createTransactionFetch(date, recipient, contact, amount, fund_id, notes) {
+  
+  const bodyData = {date, recipient, contact, amount, fund_id, notes}
+  
+  fetch("http://localhost:3000/api/v1/transactions", {
+    method: "POST",
+    headers: {"Content-Type": "application/json"},
+    body: JSON.stringify(bodyData)
+  })
+  .then(response => response.json())
+  .then(transactionData => renderTransactionCard(transactionData.data))
 }
