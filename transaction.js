c39580c4fefd25e6d43625d9cb781906604cba4f
s-blais <stephen@s-blais.com>
2020-12-14 13:30:04 -0500

    minor refactor & cleanup

--- transaction.js
+++ transaction.js
@@ -1,13 +1,13 @@
 class Transaction {
-  constructor(transactionData) {
-    this.id = transactionData.id
-    this.date = transactionData.attributes.date
-    this.recipient = transactionData.attributes.recipient
-    this.contact = transactionData.attributes.contact
-    this.amount = transactionData.attributes.amount
-    this.notes = transactionData.attributes.notes
-    this.fund_id = transactionData.attributes.fund.id // camelCase?
-    this.fund_name = transactionData.attributes.fund.name // camelCase?
+  constructor(transaction) {
+    this.id = transaction.id
+    this.date = transaction.attributes.date
+    this.recipient = transaction.attributes.recipient
+    this.contact = transaction.attributes.contact
+    this.amount = transaction.attributes.amount
+    this.notes = transaction.attributes.notes
+    this.fundId = transaction.attributes.fund.id
+    this.fundName = transaction.attributes.fund.name
     Transaction.all.push(this)
   }
   render() {
@@ -15,17 +15,16 @@ class Transaction {
     if (newItemFlag) {
       details.classList.add('new')
     }
-    const detailsContent = `
+    details.innerHTML = `
       <summary><b>
         ${this.date} – ${this.recipient} – $${this.amount}
       </b></summary>
       <p>
-        <strong>Fund:</strong> ${this.fund_name}<br>
+        <strong>Fund:</strong> ${this.fundName}<br>
         <strong>Contact:</strong> ${this.contact}<br>
         <strong>Notes:</strong> ${this.notes}
       </p>
       `
-    details.innerHTML = detailsContent
     document.getElementById('transaction-cards').prepend(details)
   }
 }
